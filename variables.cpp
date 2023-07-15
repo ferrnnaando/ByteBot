f9486626ab464b751380fb105a23b8e790dc7c72
Fernando Azuaga <77246868+ferrnnaando@users.noreply.github.com>
2023-07-15 17:26:55 +0200

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,11 @@
+#include "header.h"
+
+const uint64_t ec_default = dpp::colors::pink;
+const uint64_t ec_error = dpp::colors::red_blood; //embed color
+const std::string discord_link_inv = "https://discord.gg/bYDhwFFVk5";
+const std::string banner_url = "https://i.imgur.com/QN4KH0H.png";
+const std::string logo_url = "https://i.imgur.com/pLxW45q.png";
+const std::string blacklisted_users[3] = { "", "921516505735262251", "3" };
+
+const std::string description_slashcmd_report = "Envia un mensaje de error o queja de forma directa al desarrollador de ByteBot";
+const std::string option_usuario_slashcmd_report = "Envia un mensaje a los desarrolladores, un uso inpropio conlleva la prohibicìón";
