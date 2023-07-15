56b7045150d763cbbf0069fa787a98c5105c1141
Fernando Azuaga <77246868+ferrnnaando@users.noreply.github.com>
2023-07-15 17:21:03 +0200

    Update ByteBot.cpp

--- ByteBot.cpp
+++ ByteBot.cpp
@@ -2,8 +2,6 @@
 #include "variables.cpp"
 #include "functions.cpp"
 
-using namespace dpp;
-
 void slashcmd_reg(const dpp::interaction& interaction) {
 	std::cout << "[" + dpp::utility::current_date_time() + "] - " << interaction.usr.username << " || Slashcommand /" << interaction.get_command_name() << " ejecutado." << std::endl;
 }
