b61943325132e605223ac53523d625c48e57cbe4
Fernando Azuaga <77246868+ferrnnaando@users.noreply.github.com>
2023-07-17 22:11:29 +0200

    Add files via upload

---
+++ headers/mainheader.h
@@ -0,0 +1,14 @@
+#pragma once
+#pragma warning(disable : 4996)
+#include <iostream>
+#include <string>
+#include "dpp/dpp.h"
+#include <ctime>
+#include <time.h>
+#include <iomanip>
+#include <chrono>
+
+using namespace std;
+using namespace dpp;
+
+const std::string BOT_TOKEN = "MTEyNjY5MTc3MTUwNjc1NzcxMw.GzSkvV.3azHDeyTxP75TN9_KEs3G9vjgyGiiqr3sHnnrU";
