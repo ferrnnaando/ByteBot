3c9c61a91adb522a8b299ee28f92f30a9180398d
Fernando Azuaga <77246868+ferrnnaando@users.noreply.github.com>
2023-07-24 14:42:05 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-execname=main
+if [ -z "$1" ]; then
+    echo "enter a name for the program to be compiled."
+else
+    g++ main.cpp register/slashcommands/slashcommand_on_ready/slashcommands.cpp register/slashcommands/slashcommand_logger/slashcommand_logger.cpp register/buttons/on_button_click/on_button_click.cpp register/embeds/embed_declarations.cpp register/on_messages/on_messages.cpp -o $1 -ldpp -I dotenv_library/include/laserpants/dotenv
 
-g++ main.cpp register/slashcommands/slashcommand_on_ready/slashcommands.cpp register/slashcommands/slashcommand_logger/slashcommand_logger.cpp register/buttons/on_button_click/on_button_click.cpp register/embeds/embed_declarations.cpp register/on_messages/on_messages.cpp -o $execname -ldpp -I dotenv_library/include/laserpants/dotenv
+    chmod +x main
+    echo "compiled with exec permissions on $1"
 
-chmod +x main
-echo "compiled on $execname"
+fi
