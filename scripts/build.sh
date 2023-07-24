a5f7fe70e114780d0a95765402c4eb353bdce926
Fernando Azuaga <77246868+ferrnnaando@users.noreply.github.com>
2023-07-24 05:24:49 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-g++ main.cpp register/slashcommands/slashcommand_on_ready/slashcommands.cpp register/slashcommands/slashcommand_logger/slashcommand_logger.cpp register/buttons/on_button_click/on_button_click.cpp register/embeds/embed_declarations.cpp register/on_messages/on_messages.cpp -o main -ldpp -I dotenv_library/include/laserpants/dotenv
+execname=main
+
+g++ main.cpp register/slashcommands/slashcommand_on_ready/slashcommands.cpp register/slashcommands/slashcommand_logger/slashcommand_logger.cpp register/buttons/on_button_click/on_button_click.cpp register/embeds/embed_declarations.cpp register/on_messages/on_messages.cpp -o $execname -ldpp -I dotenv_library/include/laserpants/dotenv
 
 chmod +x main
-echo "compiled"
+echo "compiled on $execname"
