22d34f164f52e4e24d8a860c327897e1d8610a53
ferrnnaando <fernindato@gmail.com>
2023-07-27 18:46:29 +0200

    added autostart for build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,10 @@
 if [ -z "$1" ]; then
     echo "enter a name for the program to be compiled."
 else
+    rm $1
     g++ -std=c++17 main.cpp register/slashcommands/slashcommand_on_ready/slashcommands.cpp register/slashcommands/slashcommand_logger/slashcommand_logger.cpp register/buttons/on_button_click/on_button_click.cpp register/embeds/embed_declarations.cpp register/on_messages/on_messages.cpp -o $1 -ldpp -I dotenv_library/include/laserpants/dotenv
 
     chmod +x main
     echo "compiled with exec permissions on $1"
-
+    ./$1
 fi
