0164a4c741a18ee73388fc18b801cfc2ba7f22fa
ferrnnaando <fernindato@gmail.com>
2023-07-28 02:31:09 +0200

    added big-support for all compilers

--- headers/mainheader.h
+++ headers/mainheader.h
@@ -1,30 +1,25 @@
 #pragma warning(disable : 4996)
 
-#ifdef __linux__
-	#include <unistd.h>
-#elif defined(_WIN32) || defined(_WIN64)
-	#include <Windows.h>
-
-#endif
 
 #ifdef __GNUC__ || _MSC_VER
     #pragma once
-    #include <iostream>
-    #include <fstream>
-    #include <string>
-    #include <dotenv.h>
-    #include "dpp/dpp.h"
-    #include "../variables/variables.cpp"
+   
 #elif
     #ifdef MAINHEADER_H
     #define MAINHEADER_H
 
+    #endif MAINHEADER_H
+#endif
+
+#ifdef __linux__
+	#include <unistd.h>
+#elif defined(_WIN32) || defined(_WIN64)
+	#include <Windows.h>
+#endif
+
     #include <iostream>
     #include <fstream>
     #include <string>
     #include <dotenv.h>
     #include "dpp/dpp.h"
     #include "../variables/variables.cpp"
-
-    #endif MAINHEADER_H
-#endif
