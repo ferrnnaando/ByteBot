acaeb85d40568f8fe588dc23376162d8de6b6689
ferrnnaando <fernindato@gmail.com>
2023-08-03 23:08:53 +0200

    added all commands descriptions to variables cpp and set dynamic arrays

--- variables/variables.cpp
+++ variables/variables.cpp
@@ -2,13 +2,23 @@
 #include "dpp/dpp.h"
 
 enum flags {
-	ephemeral = dpp::m_ephemeral,
-	label(std::string text) = dpp::component().set_label(text)
+	ephemeral = dpp::m_ephemeral/*,
+	label(std::string text) = dpp::component().set_label(text)*/
 };
 
 const uint64_t ec_default = dpp::colors::pink;
 const uint64_t ec_error = dpp::colors::red_blood;
 const uint64_t ec_slashcmd_devusage = dpp::colors::green;
+ 
+/*asm volatile (
+        "mov %1, %%eax\n"   // Move the value into the EAX register
+        "add $1, %%eax\n"   // Add 1 to the value in EAX
+        "mov %%eax, %0"     // Move the result from EAX to the 'result' variable
+        : "=r" (result)     // Output operand (result)
+        : "r" (value)       // Input operand (value)
+        : "%eax"            // Clobbered register (EAX)
+    );
+*/
 
 const std::string application_id = "1126691771506757713";
 const std::string reports_webhook = "https://discord.com/api/webhooks/1129797836255547443/NJimRvFP1yjR5HKbAkczyA07QBcQP-6zEKmen-PimQtyCPQo2rmjrFujrLfjNJ7Gz9sf";
@@ -17,9 +27,9 @@ const std::string discord_link_inv = "https://discord.gg/bYDhwFFVk5";
 const std::string banner_url = "https://i.imgur.com/QN4KH0H.png";
 const std::string logo_url = "https://i.imgur.com/pLxW45q.png";
 
-const std::string blacklisted_users[] = { "921516505735262251" };
-const std::string blacklisted_servers[] = { "1130583864818016278"};
-const std::string bytebot_developers[] = { "1068126654368583770"};
+const std::string blacklisted_users[] = { "921535262251" };
+const std::string bytebot_developers[] = { "1068126654368583770", "1050059881043988600"};
+//const std::string blacklisted_servers[] = { "1130583864818016278"};
 //const dpp::guild* g = dpp::find_guild(blacklisted_servers[0]);
 
 const std::string description_slashcmd_report = "Envia un mensaje a los desarrolladores, un uso inpropio conlleva la prohibicìón";
