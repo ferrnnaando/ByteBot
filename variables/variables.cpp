2b8f740343f2d09def9e8bc4d6dd9c89c9659158
ferrnnaando <fernindato@gmail.com>
2023-08-03 19:18:01 +0200

    setted dinamyc arrays, removed message prefix for old monitoring

--- variables/variables.cpp
+++ variables/variables.cpp
@@ -17,10 +17,9 @@ const std::string discord_link_inv = "https://discord.gg/bYDhwFFVk5";
 const std::string banner_url = "https://i.imgur.com/QN4KH0H.png";
 const std::string logo_url = "https://i.imgur.com/pLxW45q.png";
 
-const std::string blacklisted_users[3] = { "", "921516505735262251", "3" };
-const std::string blacklisted_servers[1] = { "1130583864818016278"};
-const std::string bytebot_developers[1] = { "1068126654368583770"};
-const std::string devcommands_prefix = "###";
+const std::string blacklisted_users[] = { "921516505735262251" };
+const std::string blacklisted_servers[] = { "1130583864818016278"};
+const std::string bytebot_developers[] = { "1068126654368583770"};
 //const dpp::guild* g = dpp::find_guild(blacklisted_servers[0]);
 
 const std::string description_slashcmd_report = "Envia un mensaje a los desarrolladores, un uso inpropio conlleva la prohibicìón";
