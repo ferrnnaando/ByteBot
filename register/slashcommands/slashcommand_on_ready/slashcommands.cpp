fe42e9336ac4f44ee33e36c056e552da3215b213
ferrnnaando <fernindato@gmail.com>
2023-08-03 19:11:22 +0200

    added all the 'new' commands to variables.cpp

--- register/slashcommands/slashcommand_on_ready/slashcommands.cpp
+++ register/slashcommands/slashcommand_on_ready/slashcommands.cpp
@@ -3,19 +3,24 @@
 using namespace std;
 using namespace dpp;
 
-void print_logger(dpp::cluster& bytebot) {
-	#ifdef __linux__
-			std::cout << "[" << utility::current_date_time() << "]" << " INFO: Running on Linux" << std::endl;
-		#elif defined(_WIN32) || defined(_WIN64)
-			std::cout << "[" << utility::current_date_time() << "]" << " INFO: Running on Windows" << std::endl;
-		#endif
+void print_logger(dpp::cluster &bytebot)
+{
+#ifdef __linux__
+	std::cout << "[" << utility::current_date_time() << "]"
+			  << " INFO: Running on Linux" << std::endl;
+#elif defined(_WIN32) || defined(_WIN64)
+	std::cout << "[" << utility::current_date_time() << "]"
+			  << " INFO: Running on Windows" << std::endl;
+#endif
 
-		std::cout << "[" << utility::current_date_time() << "] INFO: Default OAuth2 URL is " << utility::bot_invite_url(application_id) << std::endl;
+	std::cout << "[" << utility::current_date_time() << "] INFO: Default OAuth2 URL is " << utility::bot_invite_url(application_id) << std::endl;
 }
 
-void on_ready(dpp::cluster& bytebot) {
-	bytebot.set_presence(presence(ps_idle, at_listening, "/bytebot en " + std::to_string(bytebot.current_user_get_guilds_sync().size()) + " servidores")); //define bot status	
-	if (run_once<struct register_bot_commands>()) { //Avoid re-running on-ready declaration everytime that the bots makes a full reconnection
+void on_ready(dpp::cluster &bytebot)
+{
+	bytebot.set_presence(presence(ps_idle, at_listening, "/bytebot en " + std::to_string(bytebot.current_user_get_guilds_sync().size()) + " servidores")); // define bot status
+	if (run_once<struct register_bot_commands>())
+	{ // Avoid re-running on-ready declaration everytime that the bots makes a full reconnection
 		dpp::slashcommand informacion("informacion", "Comando principal de ByteBot el cual te dira que hacer conmigo", bytebot.me.id);
 		dpp::slashcommand comandos("comandos", "Muestra de lo que soy capaz", bytebot.me.id);
 
@@ -23,20 +28,25 @@ void on_ready(dpp::cluster& bytebot) {
 		infousuario.add_option(dpp::command_option(co_user, "usuario", option_slashcmd_infousuario, false));
 
 		dpp::slashcommand infoservidor("infoservidor", description_slashcmd_infoservidor, bytebot.me.id);
-		infoservidor.add_option(dpp::command_option(co_string, "id", "Muestra la informacion sobre un servidor en especifico", false));
+		infoservidor.add_option(dpp::command_option(co_string, "id", option_slashcmd_infoservidor, false));
 
-		dpp::slashcommand avatar("avatar", option_usuario_slashcmd_avatar, bytebot.me.id);
-		avatar.add_option(dpp::command_option(co_user, "usuario", description_slashcmd_avatar, false));
+		dpp::slashcommand avatar("avatar", description_slashcmd_avatar, bytebot.me.id);
+		avatar.add_option(dpp::command_option(co_user, "usuario", option_slashcmd_avatar, false));
 
-		dpp::slashcommand banear("banear", "Banea a un usuario del servidor", bytebot.me.id);
-		banear.add_option(dpp::command_option(co_user, "usuario", "Especifica al usuario que deseas banear.", true)
-			.add_option(dpp::command_option(co_string, "motivo", "Especifica un motivo por el que banear al usuario.", true)));
+		dpp::slashcommand banear("banear", description_slashcmd_banear, bytebot.me.id);
+		banear.add_option(dpp::command_option(co_user, "usuario", option_slashcmd_banear, true)
+			  .add_option(dpp::command_option(co_string, "motivo", option2_slashcmd_infoservidor, true)));
 
 		slashcommand reportar("reportar", description_slashcmd_report, bytebot.me.id);
-		reportar.add_option(dpp::command_option(co_string, "mensaje", option_usuario_slashcmd_report, true));
+		reportar.add_option(dpp::command_option(co_string, "mensaje", option_slashcmd_report, true));
 
-		slashcommand dev("dev", "Consola para desarolladores de ByteBot", bytebot.me.id);
-		dev.add_option(dpp::command_option(co_string, "input", "Muestra el output", true));
+		slashcommand admin("admin", description_slashcmd_admin, bytebot.me.id);
+		admin.add_option(dpp::command_option(dpp::co_sub_command, "add", "Crea cualquier cosa que imagines para darle utilidad a tu servidor e interacción.", false)
+							 //.add_option(dpp::command_option_choice("aa", "aa", "aa", false)));
+							 .add_option(dpp::command_option(dpp::co_boolean, "ar_button", "aa", true)));
+
+		slashcommand dev("dev", description_slashcmd_dev, bytebot.me.id);
+		dev.add_option(dpp::command_option(co_string, "input", option_slashcmd_dev, true));
 
 		bytebot.global_command_create(informacion);
 		bytebot.global_command_create(comandos);
@@ -45,8 +55,9 @@ void on_ready(dpp::cluster& bytebot) {
 		bytebot.global_command_create(avatar);
 		bytebot.global_command_create(banear);
 		bytebot.global_command_create(reportar);
+		bytebot.global_command_create(admin);
 		bytebot.global_command_create(dev);
-		//bytebot.guild_command_create(banearte);
-		//bytebot.global_bulk_command_create({ comandos, informacion, infousuario, infoservidor, avatar, banear , reportar });
-		}
+		// bytebot.guild_command_create(banearte);
+		// bytebot.global_bulk_command_create({ comandos, informacion, infousuario, infoservidor, avatar, banear , reportar });
+	}
 }
--- variables/variables.cpp
+++ variables/variables.cpp
@@ -2,8 +2,8 @@
 #include "dpp/dpp.h"
 
 enum flags {
-	ephemeral = dpp::m_ephemeral//,
-	//label = dpp::component().set_label("test")
+	ephemeral = dpp::m_ephemeral,
+	label(std::string text) = dpp::component().set_label(text)
 };
 
 const uint64_t ec_default = dpp::colors::pink;
@@ -23,16 +23,23 @@ const std::string bytebot_developers[1] = { "1068126654368583770"};
 const std::string devcommands_prefix = "###";
 //const dpp::guild* g = dpp::find_guild(blacklisted_servers[0]);
 
-const std::string option_usuario_slashcmd_report = "Envia un mensaje a los desarrolladores, un uso inpropio conlleva la prohibicìón";
-const std::string description_slashcmd_report = "Envia un mensaje de error o queja de forma directa al desarrollador de ByteBot";
+const std::string description_slashcmd_report = "Envia un mensaje a los desarrolladores, un uso inpropio conlleva la prohibicìón";
+const std::string option_slashcmd_report = "Especifica el mensaje que deseas enviar.";
 
-const std::string description_slashcmd_ban = "Banea a un usuario del servidor";
-const std::string option_usuario_slashcmd_ban = "Banea a un usuario del servidor";
-
-const std::string description_slashcmd_avatar = "Usuario del que mostrar su foto de perfil.";
-const std::string option_usuario_slashcmd_avatar = "Muestra la foto de perfil de alguien.";
+const std::string description_slashcmd_avatar = "Muestra tu foto de perfil.";
+const std::string option_slashcmd_avatar = "Usuario del que mostrar su foto de perfil.";
 
 const std::string description_slashcmd_infousuario = "Muestra toda la información a mi disposición sobre un usuario.";
 const std::string option_slashcmd_infousuario = "Usuario del que quieras saber mas.";
 
 const std::string description_slashcmd_infoservidor = "Muestra toda la información a mi disposición sobre el servidor en el que se ejecuta el comando";
+const std::string option_slashcmd_infoservidor = "Muestra la informacion sobre un servidor en especifico";
+
+const std::string description_slashcmd_banear = "Banea a un usuario del servidor";
+const std::string option_slashcmd_banear = "Especifica al usuario que deseas banear.";
+const std::string option2_slashcmd_infoservidor = "Especifica un motivo por el que banear al usuario.";
+
+const std::string description_slashcmd_admin = "Comando destinado a los administradores y moderadores de comunidades.";
+
+const std::string description_slashcmd_dev = "Consola para desarolladores de ByteBot";
+const std::string option_slashcmd_dev = "Muestra el output";
